cadcfaec3323b045592e1ef6a3c166f8a0b40e46
kpao2020 <xoakuma@gmail.com>
2024-02-28 13:10:25 -0800

    update index.html and p2.js

--- p2.js
+++ p2.js
@@ -1,6 +1,6 @@
 // Team : Ken Pao, Yuying Huang
 // Class: ART 259
-// Title: Bunny Jump
+// Title: <tbd>
 // P5JS link: <tbd>
 // Reference: listed at the end of this file
 ///////////////////////////////////////////////////////////////////////////////
@@ -14,10 +14,15 @@ function preload(){
 
 // Initialize setup
 function setup(){
-
+    new Canvas(windowWidth*0.8,windowHeight*0.8);
+    world.gravity.y = 5;
+    floor = new Sprite(0, height*0.8, width, 10, 's');
+    p = new Sprite()
 }
 
 // Draw 
 function draw(){
-    
+    clear();
+
+
 }
