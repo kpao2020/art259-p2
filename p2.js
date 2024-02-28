0c8e357ef89dd7b1fd9744896f1e4c5ff4478cb9
kpao2020 <xoakuma@gmail.com>
2024-02-28 11:02:22 -0800

    first commit"

---
+++ p2.js
@@ -0,0 +1,23 @@
+// Team : Ken Pao, Yuying Huang
+// Class: ART 259
+// Title: Bunny Jump
+// P5JS link: <tbd>
+// Reference: listed at the end of this file
+///////////////////////////////////////////////////////////////////////////////
+
+// Global Variables
+
+// Preload media
+function preload(){
+
+}
+
+// Initialize setup
+function setup(){
+
+}
+
+// Draw 
+function draw(){
+    
+}
